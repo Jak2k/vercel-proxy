87b77f14fcfd7d9d1d1f85fefa6e6e780337dad3
Jak2k <76660118+Jak2k@users.noreply.github.com>
2023-07-31 13:04:58 +0000

    add proxy functionality

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,14 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { get } from '@vercel/edge-config';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const response = await fetch(get("host", "https://example.com")+req.query.match);
+  const data = await response.text()
+
   return res.json({
     query: {...req.query, match: undefined},
     body: req.body,
-    url: req.query.match|| "/"
+    url: req.query.match|| "/",
+    data
   })
 }
