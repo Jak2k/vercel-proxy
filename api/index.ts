958bd31ce299ddd65ead842e43d2015a664c4e24
Jak2k <76660118+Jak2k@users.noreply.github.com>
2023-07-31 12:56:13 +0000

    add url fallback

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,6 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
   return res.json({
     query: {...req.query, match: undefined},
     body: req.body,
-    url: req.query.match
+    url: req.query.match|| "/"
   })
 }
