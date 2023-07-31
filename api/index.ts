7f63ce8a9025aecafab8331559c5594c2a1e7a6d
Jak2k <76660118+Jak2k@users.noreply.github.com>
2023-07-31 12:48:37 +0000

    fix: syntax error

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.
   return res.json({
     query: req.query,
     body: req.body,
